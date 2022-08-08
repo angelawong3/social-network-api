dd5a4fe66d113a1c110f0315d6035a41d3ff8c3a
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-08-08 19:20:17 +0100

    Thought model done

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,42 @@
 const { Schema, model } = require("mongoose");
-const Reaction = require("./Reaction");
+const moment = require("moment");
 
-const thoughtSchema = new Schema();
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getter: true,
+    },
+    id: false,
+  }
+);
+
+// virtual to count reactions
+thoughtSchema.virtual("reactionsCount").get(function () {
+  return this.reactions.length;
+});
+
+// Initialize our Thought model
+const Thought = model("thought", thoughtSchema);
 
 module.exports = Thought;
