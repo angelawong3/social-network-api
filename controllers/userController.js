32853a5ff436745f78391aab1f3140a2235c41bb
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-08-10 13:13:29 +0100

    get and post req done and tested

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,37 @@
 const User = require("../models/User");
 
-module.exports = {};
+module.exports = {
+  // get all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // get a single user by id
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // update a user by id
+
+  // del a user by id
+
+  // add a new friend
+
+  // del a friend
+};
