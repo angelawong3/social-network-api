2c11039b88812a6488d8cc402159dd3649f6f653
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-08-10 13:58:36 +0100

    thoughts route all done and tested

--- models/Thought.js
+++ models/Thought.js
@@ -1,36 +1,6 @@
 const { Schema, model, Types } = require("mongoose");
 const moment = require("moment");
 
-// Schema to create Thought model
-const thoughtSchema = new Schema(
-  {
-    thoughtText: {
-      type: String,
-      required: true,
-      minLength: 1,
-      maxLength: 280,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAtVal) =>
-        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    reactions: [reactionSchema],
-  },
-  {
-    toJSON: {
-      virtuals: true,
-      getter: true,
-    },
-    id: false,
-  }
-);
-
 // reaction schema
 const reactionSchema = new Schema(
   {
@@ -58,6 +28,36 @@ const reactionSchema = new Schema(
     toJSON: {
       getters: true,
     },
+  }
+);
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getter: true,
+    },
+    id: false,
   }
 );
 
